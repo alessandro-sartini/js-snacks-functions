d6bdacf4e0534e7df9f5282d1142d04053aaf5a8
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-16 12:19:01 +0100

    first commit

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+
+function concatString(string) {
+    
+console.log(`Ciao ${string}`)
+
+}
+// Invoca la funzione qui e stampa il risultato in console
+
+concatString(userName)
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
