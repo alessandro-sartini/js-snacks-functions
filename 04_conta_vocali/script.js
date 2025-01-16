9a23c57862f8bf1a9a293dde668c5f4c0e6476ab
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-16 13:46:20 +0100

    ex 4 ok complete

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/* Scrivi una funzione che accetti una stringa e restituisca 
+il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numberChar(stringCharCount) {
+    let numVocali=0;
+    const vocali = ['a', 'e', 'i', 'o', 'u', 'y'];
+    for (let i = 0; i < stringCharCount.length; i++){
+        if (vocali.includes(stringCharCount[i].toLowerCase())) {
+            numVocali++
+        }
 
+    }
+    return numVocali
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(numberChar(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
