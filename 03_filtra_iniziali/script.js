41e41a8a5d657edf99ffed0771d05eefb27daefe
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-16 12:57:18 +0100

    3st ex ok

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function startLetter(array,char) {
+   
+    let selectedLetter=[]
+    for (let i = 0; i < array.length; i++){
+        if (char.toLowerCase() === array[i].charAt(0).toLowerCase()){
+                
+            selectedLetter.push( array[i]);
 
+        } 
+    }
 
+    if (selectedLetter.length === 0) {
+        console.log(`Non ci sono nomi che iniziano per ${char}`);
+    }
+    return selectedLetter;
+    
+    
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(startLetter(names, "a"))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
